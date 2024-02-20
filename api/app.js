e1f3d10d3206a3d29839553cb19275139842394c
Siddhant Chavadiya <69337069+siddhantchavadiya@users.noreply.github.com>
2024-02-20 18:41:46 +0530

    Update app.js

--- api/app.js
+++ api/app.js
@@ -5,7 +5,7 @@ const app = express();
 const port = process.env.PORT || 4000;
 
 app.get('/', (req, res) => {
-  res.send('Home Route Backend is running.');
+  res.send('Home Route Backend is running using cicd.');
 });
 
 app.listen(port, () =>
